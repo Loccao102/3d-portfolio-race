f8828d4097564788edc7b7a1482561c3121b994f
Loccao102 <138382005+Loccao102@users.noreply.github.com>
2026-09-11 14:31:45 +0700

    refactor: route portfolio content through feature boundary

--- src/game/world/WorldPhysics.tsx
+++ src/game/world/WorldPhysics.tsx
@@ -2,11 +2,7 @@ import React, { Suspense } from 'react';
 import { Physics, type RapierRigidBody } from '@react-three/rapier';
 import { Ground } from '@/components/world/Ground';
 import { InstancedProps } from '@/components/world/InstancedProps';
-import { AboutDistrict } from '@/components/world/districts/AboutDistrict';
-import { TechDistrict } from '@/components/world/districts/TechDistrict';
-import { ProjectGarage } from '@/components/world/districts/ProjectGarage';
-import { ExperimentLab } from '@/components/world/districts/ExperimentLab';
-import { ContactStation } from '@/components/world/districts/ContactStation';
+import { PortfolioDistricts } from '../features/portfolio/PortfolioDistricts';
 import { LocalPlayer } from '../entities/player/LocalPlayer';
 import { PHYSICS_CONFIG } from '../config/scene';
 
@@ -24,13 +20,7 @@ export function WorldPhysics({ playerRef }: WorldPhysicsProps) {
       <Physics gravity={PHYSICS_CONFIG.gravity} timeStep={PHYSICS_CONFIG.timeStep}>
         <Ground />
         <InstancedProps />
-
-        <AboutDistrict />
-        <TechDistrict />
-        <ProjectGarage />
-        <ExperimentLab />
-        <ContactStation />
-
+        <PortfolioDistricts />
         <LocalPlayer ref={playerRef} />
       </Physics>
     </Suspense>
